3749cc46994fecaaf40d0e5894c903844475fac5
Yauheni Yeremetsko <yauheni.yeremetsko@innowise-group.com>
2020-09-30 16:52:29 +0300

    deploy

---
+++ precache-manifest.959ea292a6b838ad4608983dc946fb38.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c5fe3727126b5bd77d",
+    "url": "/firebase/css/app.14d96648.css"
+  },
+  {
+    "revision": "a1b14c6dd665050de986",
+    "url": "/firebase/css/chunk-62b315f4.3699aaba.css"
+  },
+  {
+    "revision": "4a7c688005dd936b2fac",
+    "url": "/firebase/css/chunk-bd8e6dba.d52d5dbc.css"
+  },
+  {
+    "revision": "f85572604cab679eb8cd",
+    "url": "/firebase/css/chunk-vendors.8d65f6d3.css"
+  },
+  {
+    "revision": "c8388fe02319ba25fc1ebd4e4b9fc29d",
+    "url": "/firebase/index.html"
+  },
+  {
+    "revision": "68c5fe3727126b5bd77d",
+    "url": "/firebase/js/app.e3912b06.js"
+  },
+  {
+    "revision": "a1b14c6dd665050de986",
+    "url": "/firebase/js/chunk-62b315f4.b8c97010.js"
+  },
+  {
+    "revision": "4a7c688005dd936b2fac",
+    "url": "/firebase/js/chunk-bd8e6dba.385a8f8d.js"
+  },
+  {
+    "revision": "f85572604cab679eb8cd",
+    "url": "/firebase/js/chunk-vendors.ee9991d5.js"
+  },
+  {
+    "revision": "2a161e29fb2f237e513b7f8f02700734",
+    "url": "/firebase/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/firebase/robots.txt"
+  }
+]);
